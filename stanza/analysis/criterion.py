de305d3ea635e4461150bde0052358802499c407
Raymond Jow <raymond@conductorquantum.com>
2025-10-10 15:54:24 -0700

    add characterization tests

--- stanza/analysis/criterion.py
+++ stanza/analysis/criterion.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 
-def pcov_fail_criterion(pcov: np.ndarray, ubound: float = 1e-10) -> bool:
+def pcov_fail_criterion(pcov: np.ndarray, ubound: float = 1e-8) -> bool:
     """Fail criterion for the covariance matrix of the fitted parameters."""
-    return bool(np.any(np.diag(pcov) > ubound) or np.allclose(pcov, 0.0))
+    return bool(np.any(np.diag(pcov) > ubound) or np.allclose(pcov, 0.0, atol=1e-14))
